12243866d804d860a54ac2994d70e21cd835f674
Ashut+osh-Tosh <ashutoshdp49@gmail.com>
2022-07-11 19:12:47 +0530

    Updates

---
+++ precache-manifest.fa734dec98b92dba13466d732dfe8d5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fb787427f411349e1db",
+    "url": "/MyFirstReactApp-ROBOFRIENDS-/static/css/main.c2ebce2a.chunk.css"
+  },
+  {
+    "revision": "0fb787427f411349e1db",
+    "url": "/MyFirstReactApp-ROBOFRIENDS-/static/js/main.0fb78742.chunk.js"
+  },
+  {
+    "revision": "c41c1c1a55ea0027eb7b",
+    "url": "/MyFirstReactApp-ROBOFRIENDS-/static/js/1.c41c1c1a.chunk.js"
+  },
+  {
+    "revision": "cdc01a668121d6c52ac7",
+    "url": "/MyFirstReactApp-ROBOFRIENDS-/static/css/2.1f064f68.chunk.css"
+  },
+  {
+    "revision": "cdc01a668121d6c52ac7",
+    "url": "/MyFirstReactApp-ROBOFRIENDS-/static/js/2.cdc01a66.chunk.js"
+  },
+  {
+    "revision": "c0e7ce78d311b7f90ad9",
+    "url": "/MyFirstReactApp-ROBOFRIENDS-/static/js/runtime~main.c0e7ce78.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/MyFirstReactApp-ROBOFRIENDS-/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8c9f94d9b2abbaa4ab7654b67da1bfb5",
+    "url": "/MyFirstReactApp-ROBOFRIENDS-/index.html"
+  }
+];
